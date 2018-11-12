2cc8ea497489cbaac9de0262c9d028e91274b73f
Eden Yefet <edenyefet@gmail.com>
2018-11-12 20:34:54 +0200

    A Star done.

--- py_ex1.py
+++ py_ex1.py
@@ -1,14 +1,31 @@
+import datetime
 import sys
 from collections import deque
 
 
 class Node:
-    def __init__(self, state, depth, operator=None, parent=None):
+    def __init__(self, state, operator=None, parent=None, g=float('inf'), h=float('inf')):
         self.state = state
         self.operator = operator
-        self.children = []
         self.parent = parent
-        self.depth = depth
+        self.g = g
+        self.h = h
+
+    @property
+    def f(self):
+        return self.g + self.h
+
+    def __hash__(self):
+        return hash(self.state_string())
+
+    def __eq__(self, other):
+        return self.state == other.state
+
+    def __repr__(self):
+        return '<' + self.state_string() + '>'
+
+    def __lt__(self, other):
+        return self.h + self.g < other.h + other.g
 
     def state_string(self):
         return ','.join(str(x) for x in self.state)
@@ -27,40 +44,41 @@ class TilePuzzle:
                            3: Algorithm(self._a_star, 'A*')}
         self.goal = [x for x in range(1, self.size ** 2)]
         self.goal.append(0)
+        self.length = self.size ** 2
 
     def print_board(self):
+        print('Initial State:')
         for i in range(0, self.size * self.size, self.size):
-            print(self.root.state[i:i + self.size])
+            print('  '.join([str(x) for x in self.root.state[i:i + self.size]]))
+        print('')
 
     def _successors(self, node):
         successors = []
-        last_operator = 'G'  # node.operator
         z = node.state.index(0)
-        length = self.size ** 2
 
         # Up
-        if z + self.size < length and last_operator != 'D':
+        if z + self.size < self.length:
             state_copy = list(node.state)
             state_copy[z], state_copy[z + self.size] = state_copy[z + self.size], state_copy[z]
-            successors.append(Node(state_copy, node.depth + 1, 'U', node))
+            successors.append(Node(state_copy, 'U', node))
 
         # Down
-        if z - self.size >= 0 and last_operator != 'U':
+        if z - self.size >= 0:
             state_copy = list(node.state)
             state_copy[z], state_copy[z - self.size] = state_copy[z - self.size], state_copy[z]
-            successors.append(Node(state_copy, node.depth + 1, 'D', node))
+            successors.append(Node(state_copy, 'D', node))
 
         # Left
-        if z % self.size + 1 < self.size and last_operator != 'R':
+        if z % self.size + 1 < self.size:
             state_copy = list(node.state)
             state_copy[z], state_copy[z + 1] = state_copy[z + 1], state_copy[z]
-            successors.append(Node(state_copy, node.depth + 1, 'L', node))
+            successors.append(Node(state_copy, 'L', node))
 
         # Right
-        if z % self.size - 1 >= 0 and last_operator != 'L':
+        if z % self.size - 1 >= 0:
             state_copy = list(node.state)
             state_copy[z], state_copy[z - 1] = state_copy[z - 1], state_copy[z]
-            successors.append(Node(state_copy, node.depth + 1, 'R', node))
+            successors.append(Node(state_copy, 'R', node))
 
         return successors
 
@@ -71,6 +89,8 @@ class TilePuzzle:
             path.append(current.operator)
             current = current.parent
         path.reverse()
+        if len(path) == 0:
+            return 'NOTHING'
         return ''.join(path)
 
     @staticmethod
@@ -87,20 +107,48 @@ class TilePuzzle:
     def solve(self):
         if self.algorithm_num in self.algorithms:
             algorithm = self.algorithms[self.algorithm_num]
-            print('Solving with ' + algorithm.name + '...')
-            return algorithm.method()
+            start_time = datetime.datetime.today()
+            print(str(start_time) + ': Solving with ' + algorithm.name + '...')
+            result = algorithm.method()
+            end_time = datetime.datetime.today()
+            print (str(end_time) + ': Finished in ' + str(end_time - start_time) + '.')
+            return result
         else:
             raise Exception('An algorithm with number ' + str(self.algorithm_num) + ' was not found.')
 
     def _ids(self):
         max_depth = 0
         while True:
-            result = self._bfs(max_depth)
-            if result[0]:
-                return result[0], result[1], max_depth
+            found, remaining = self._dls(self.root, max_depth)
+            if found:
+                return self.get_path_from_root(found), remaining, max_depth + 1
+            elif not remaining:
+                return None
             max_depth += 1
 
-    def _bfs(self, max_depth=-1):
+    def _dls(self, node, depth):
+        """
+        Depth Limited Search
+        :param node:
+        :param depth:
+        :return:
+        """
+        if depth == 0:
+            if node.state == self.goal:
+                return node, 0
+            else:
+                # Not found, but may have successors
+                return None, 1
+
+        current = 0
+        for s in self._successors(node):
+            found, remaining = self._dls(s, depth - 1)
+            current += remaining
+            if found:
+                return found, current + 1
+        return None, current
+
+    def _bfs(self):
         queue = deque([self.root])
         counter = 0
         while queue:
@@ -108,21 +156,69 @@ class TilePuzzle:
             counter += 1
             if n.state == self.goal:
                 return self.get_path_from_root(n), counter, 0
-            if n.depth == max_depth:
-                return False, -1, -1
             successors = self._successors(n)
             queue.extend(successors)
         return False, -1, -1
 
+    def _array_index_to_row_col(self, i):
+        """
+        Calculates row and column indices.
+        :param i: An index in a 1-dim array.
+        :return: A 2-tuple of (row, column) of a square matrix of size self.size.
+        """
+        return int(i / self.size), i % self.size
+
+    def _manhattan_distance(self, state_as_list):
+        distances_sum = 0
+        for current_index, n in enumerate(state_as_list):
+            if n == 0:
+                continue
+            current_row, current_column = self._array_index_to_row_col(current_index)
+            goal_row, goal_column = self._array_index_to_row_col(self.goal.index(n))
+            distances_sum += abs(goal_row - current_row) + abs(goal_column - current_column)
+
+        return distances_sum
+
     def _a_star(self):
-        pass
+        from heapq import heappush, heappop
+        opened = []
+        self.root.g = 0
+        self.root.h = self._manhattan_distance(self.root.state)
+        heappush(opened, (self.root.f, self.root))
+        closed = set()
+
+        while opened:
+            n = heappop(opened)[1]
+
+            if n.state == self.goal:
+                path = self.get_path_from_root(n)
+                return path, len(closed), len(path)
+
+            if n in closed:
+                continue
+
+            for s in self._successors(n):
+                s.g = n.g + 1
+                s.h = self._manhattan_distance(s.state)
+                heappush(opened, (s.f, s))
+            closed.add(n)
+        return False, len(closed), -1
 
 
 if __name__ == '__main__':
     t = TilePuzzle(sys.argv[1])
     t.print_board()
     path, total_opened, depth = t.solve()
-    result_string = path + ' ' + str(total_opened) + ' ' + str(depth)
+    if path:
+        result_string = path + ' ' + str(total_opened) + ' ' + str(depth)
+
+        formatted_path = path[0:4]
+        for i in range(4, len(path), 4):
+            formatted_path += '-' + path[i:i + 4]
+        print (formatted_path)
+    else:
+        result_string = 'No Solution'
+
     print(result_string)
     with open('output.txt', 'w') as f:
         f.write(result_string)
